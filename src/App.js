3028e7b5072344e98d7e8202cf72b9bb7d9be946
Sandeep Sarode <sandeepsarode21@gmail.com>
2021-10-11 14:57:29 -0400

    Added Chat and Message componets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Chat from "./Chat.js";
 
 function App() {
   return (
     <div className="App">
-      {/*Header*/}
-      <Header />
+      <Router>
+        {/*Header*/}
+        <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        {/*SideBar*/}
-        {/*React router -> Chat Screen*/}
-      </div>
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>welcome</h1>
+            </Route>
+          </Switch>
+          {/*SideBar*/}
+          {/*React router -> Chat Screen*/}
+        </div>
+      </Router>
     </div>
   );
 }
